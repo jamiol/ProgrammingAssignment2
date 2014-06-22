93858a686b8e651dc8f5a770cff6585dbbaca607
Jacob Midtgaard-Olesen <jmo@zmags.com>
2014-06-23 00:31:29 +0200

    Added logic as required by coursera/rprog-004/assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions can be used to calculate and cache matrix inverse values.
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedInverse <- NULL
+    set <- function(y) {
+        x <<- y
+        cachedInverse <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) cachedInverse <<- inverse
+    getInverse <- function() cachedInverse
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function will return the inverse of the cache matrix type (makeCacheMatrix).
+## If the inverse has previously been calculated the cached value will be returned.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if(!is.null(inverse)) {
+        message("Returning cached inverse")
+        inverse
+    }
+    data <- x$get()
+    inverse <- solve(data)
+    x$setInverse(inverse)
+    inverse
 }
